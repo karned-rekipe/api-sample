452787404592e7617016e85d4ee585903322fc63
Killian KOPP <killiankopp@gmail.com>
2025-07-24 10:45:47 +0200

    Update configuration to include Kafka settings and simplify imports

--- config/config.py
+++ config/config.py
@@ -20,5 +20,10 @@ REDIS_PORT = int(os.environ.get('REDIS_PORT', '6379'))
 REDIS_DB = int(os.environ.get('REDIS_DB', '0'))
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', 'test-password')
 
+# Kafka Configuration
+KAFKA_HOST = os.environ.get('KAFKA_HOST', 'localhost')
+KAFKA_PORT = int(os.environ.get('KAFKA_PORT', '9092'))
+KAFKA_TOPIC = os.environ.get('KAFKA_TOPIC', 'detection-topic')
+
 UNPROTECTED_PATHS = ['/favicon.ico', '/docs', '/sample/openapi.json']
 UNLICENSED_PATHS = []
